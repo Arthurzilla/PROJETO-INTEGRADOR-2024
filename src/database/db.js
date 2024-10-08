734e053a32e904724b4a1f07c55e4c96450bdad7
Luiz Augusto Silverio Zeferino <luizaugusto.szw@gmail.com>
2024-10-07 23:10:35 -0300

    adicionado mongo atlas funcional

--- src/database/db.js
+++ src/database/db.js
@@ -1,13 +1,24 @@
 const mongoose = require('mongoose');
+require('dotenv').config(); // Carrega as variáveis de ambiente do .env
 
-const connectDatabase = ()=>{
+const connectDatabase = () => {
+    console.log('Aguardando conexão...');
 
-    console.log('aguardando conexão')
+    const uri = process.env.MONGODB_URI; // Obtém a URI do banco de dados
 
-    mongoose.connect('mongodb://127.0.0.1:27017/orzh')
-    .then(() => console.log('MongoDb conectado.'))
-    .catch((error) => console.log(error));
-
+    mongoose.connect(uri)
+        .then(() => console.log('MongoDB conectado com sucesso.'))
+        .catch((error) => console.log('Erro ao conectar ao MongoDB:', error));
 }
+
+// const connectDatabase = ()=>{
+
+//     console.log('aguardando conexão')
+
+//     mongoose.connect('mongodb://127.0.0.1:27017/orzh')
+//     .then(() => console.log('MongoDb conectado.'))
+//     .catch((error) => console.log(error));
+
+// }
 
 module.exports = connectDatabase;
